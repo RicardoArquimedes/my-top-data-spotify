4b1738606662143f30fbdb385c44a1db9d3f637b
RicardoArquimedes <richardcqt28@gmail.com>
2024-04-13 17:58:56 -0500

    :construction: addning app component with the first views pages, nnavbar and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Card from './components/card/Card';
+import { getTopSongs } from './utils/spotifyApi/spotifyApi';
 
-function App() {
-  const [count, setCount] = useState(0)
+const Home: React.FC = () => {
+  const [songs, setSongs] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function fetchTopSongs() {
+      const tracks = await getTopSongs();
+      setSongs(tracks);
+    }
+    fetchTopSongs();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <Navbar />
+      <h2>Top 10 Songs</h2>
+      <div className="card-container">
+        {songs.map((song, index) => (
+          <Card 
+            key={index}
+            image={song.album.images[0].url}
+            name={song.name}
+            artist={song.artists.map((artist: any) => artist.name).join(', ')}
+            spotifyUrl={song.external_urls.spotify}
+            extraInfo={`Popularity: ${song.popularity}`}
+          />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default Home;
