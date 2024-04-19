9552750c76869914d9f8a8ac098fc06ba7a0e475
RicardoArquimedes <richardcqt28@gmail.com>
2024-04-19 17:24:33 -0500

    :construction: updating assets path

--- src/views/LoginPage/LoginPage.tsx
+++ src/views/LoginPage/LoginPage.tsx
@@ -14,7 +14,8 @@ const LoginPage: React.FC = () => {
     generateCodeChallenge(codeVerifier).then((codeChallenge) => {
       const clientId = import.meta.env.VITE_CLIENT_ID;
       const redirectUri = 
-        "http://localhost:5173/callback"
+        "https://my-top-data-spotify.vercel.app/callback"
+    
       const scopes =
        "user-top-read user-read-recently-played user-read-private user-read-email playlist-read-private user-library-read"
       const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&code_challenge_method=S256&code_challenge=${codeChallenge}&scope=${scopes}`;
